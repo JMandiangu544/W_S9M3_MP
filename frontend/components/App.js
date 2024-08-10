3e6e977c8fdc9d80b4f33452ac2ecbbe72f4dc6e
Joberthy Mandiangu <joberthymambu@gmail.com>
2024-08-09 20:19:56 -0500

    Updates

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react"
-import data from '../data/data.json'
-import axios from "axios"
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import data from '../data/data.json';
 
-import Charts from "./Charts"
-import Navbar from "./Navbar"
+import Charts from "./Charts";
+import Navbar from "./Navbar";
+import useDarkMode from '../hooks/useDarkMode';
 
 const App = () => {
-  const [coinData, setCoinData] = useState([])
-  const [darkMode, setDarkMode] = useState(false)
+  const [coinData, setCoinData] = useState([]);
+  const [darkMode, setDarkMode] = useDarkMode();
 
   useEffect(() => {
     axios
@@ -15,19 +16,20 @@ const App = () => {
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true"
       )
       .then(res => {
-        setCoinData(res.data)
+        setCoinData(res.data);
       })
       .catch(() => {
-        console.log('request failed, using fallback data')
-        setCoinData(data)
-      })
-  }, [])
+        console.log('request failed, using fallback data');
+        setCoinData(data);
+      });
+  }, []);
+
   return (
     <div className={darkMode ? "dark-mode App" : "App"}>
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <Charts coinData={coinData} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
